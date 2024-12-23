e3a2014fea9e18f41245122021111814b314367d
Christian Lønaas <christian.lonaas@ugle-z.no>
2024-12-23 14:07:21 +0100

    Create bin directory if it doesn't exist

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Download Tailwind CLI
 #
 if ! test -f bin/tailwindcss ; then
+	mkdir -p bin
 	echo "Downloading Tailwind CLI"
 	wget https://github.com/tailwindlabs/tailwindcss/releases/download/v3.4.17/tailwindcss-linux-x64 -O bin/tailwindcss
 	chmod +x bin/tailwindcss
