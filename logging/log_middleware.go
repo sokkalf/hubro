55c6b4588b3c8c3174e3042c68553be1118a4b82
Christian Lønaas <christian.lonaas@ugle-z.no>
2024-12-25 12:53:56 +0100

    Trick for allowing status to be logged

--- logging/log_middleware.go
+++ logging/log_middleware.go
@@ -9,13 +9,49 @@ import (
 	"github.com/sokkalf/hubro/server"
 )
 
+type CustomResponseWriter struct {
+    responseWriter http.ResponseWriter
+    StatusCode     int
+}
+
+func ExtendResponseWriter(w http.ResponseWriter) *CustomResponseWriter {
+    return &CustomResponseWriter{w, 0}
+}
+
+func (w *CustomResponseWriter) Write(b []byte) (int, error) {
+    return w.responseWriter.Write(b)
+}
+
+func (w *CustomResponseWriter) Header() http.Header {
+    return w.responseWriter.Header()
+}
+
+func (w *CustomResponseWriter) WriteHeader(statusCode int) {
+    // receive status code from this method
+    w.StatusCode = statusCode
+    w.responseWriter.WriteHeader(statusCode)
+
+    return
+}
+
+func (w *CustomResponseWriter) Done() {
+    // if the `w.WriteHeader` wasn't called, set status code to 200 OK
+    if w.StatusCode == 0 {
+        w.StatusCode = http.StatusOK
+    }
+
+    return
+}
+
 func LogMiddleware() server.Middleware {
 	return func(h *server.Hubro) func(http.Handler) http.Handler {
 		return func(next http.Handler) http.Handler {
 			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				start := time.Now()
-				next.ServeHTTP(w, r)
-				slog.Info(fmt.Sprintf("%s %s", r.Method, r.URL.Path), "remoteaddr", r.RemoteAddr, "time", time.Since(start))
+				ew := ExtendResponseWriter(w)
+				next.ServeHTTP(ew, r)
+				ew.Done()
+				slog.Info(fmt.Sprintf("%s %s", r.Method, r.URL.Path), "remoteaddr", r.RemoteAddr, "status", ew.StatusCode, "time", time.Since(start))
 			})
 		}
 	}
