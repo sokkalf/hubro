3bc2a995b5444ab2743262cad83125845dea0dc6
Christian Lønaas <christian.lonaas@ugle-z.no>
2024-12-25 12:55:11 +0100

    Formatting

--- logging/log_middleware.go
+++ logging/log_middleware.go
@@ -10,37 +10,37 @@ import (
 )
 
 type CustomResponseWriter struct {
-    responseWriter http.ResponseWriter
-    StatusCode     int
+	responseWriter http.ResponseWriter
+	StatusCode     int
 }
 
 func ExtendResponseWriter(w http.ResponseWriter) *CustomResponseWriter {
-    return &CustomResponseWriter{w, 0}
+	return &CustomResponseWriter{w, 0}
 }
 
 func (w *CustomResponseWriter) Write(b []byte) (int, error) {
-    return w.responseWriter.Write(b)
+	return w.responseWriter.Write(b)
 }
 
 func (w *CustomResponseWriter) Header() http.Header {
-    return w.responseWriter.Header()
+	return w.responseWriter.Header()
 }
 
 func (w *CustomResponseWriter) WriteHeader(statusCode int) {
-    // receive status code from this method
-    w.StatusCode = statusCode
-    w.responseWriter.WriteHeader(statusCode)
+	// receive status code from this method
+	w.StatusCode = statusCode
+	w.responseWriter.WriteHeader(statusCode)
 
-    return
+	return
 }
 
 func (w *CustomResponseWriter) Done() {
-    // if the `w.WriteHeader` wasn't called, set status code to 200 OK
-    if w.StatusCode == 0 {
-        w.StatusCode = http.StatusOK
-    }
+	// if the `w.WriteHeader` wasn't called, set status code to 200 OK
+	if w.StatusCode == 0 {
+		w.StatusCode = http.StatusOK
+	}
 
-    return
+	return
 }
 
 func LogMiddleware() server.Middleware {
