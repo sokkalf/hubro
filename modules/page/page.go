5a17642339d0f502bfe7e83cb15293c2ac59e9db
Christian Lønaas <christian.lonaas@ugle-z.no>
2024-12-27 00:47:20 +0100

    Add the 404 handler

--- modules/page/page.go
+++ modules/page/page.go
@@ -2,13 +2,19 @@ package page
 
 import (
 	"fmt"
+	"html"
 	"net/http"
 
 	"github.com/sokkalf/hubro/server"
 )
 
-func Register(h *server.Hubro, mux *http.ServeMux) () {
+func Register(h *server.Hubro, mux *http.ServeMux) {
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		msg := "Page not found"
+		h.ErrorHandler(w, r, http.StatusNotFound, &msg)
+		return
 	})
 }
