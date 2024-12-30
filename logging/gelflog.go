b3a3548d7ec22eabd778d39425c36703234ef621
Christian Lønaas <christian.lonaas@ugle-z.no>
2024-12-30 18:19:19 +0100

    Nanoseconds are unreadable, truncate to milliseconds

--- logging/gelflog.go
+++ logging/gelflog.go
@@ -1,14 +1,24 @@
 package logging
 
 import (
-	"os"
 	"log/slog"
+	"os"
+	"time"
 
 	"github.com/Graylog2/go-gelf/gelf"
 	sloggraylog "github.com/samber/slog-graylog/v2"
 	slogmulti "github.com/samber/slog-multi"
 )
 
+func replaceDuration(groups []string, attr slog.Attr) slog.Attr {
+	if attr.Key == "duration" {
+		if d, ok := attr.Value.Any().(time.Duration); ok {
+			ms := float64(d.Microseconds()) / 1000.0
+			attr.Value = slog.Float64Value(ms)
+		}
+	}
+	return attr
+}
 
 func InitGelfLog(logLevel slog.Level, gelfEndpoint string) {
 	gelfWriter, err := gelf.NewWriter(gelfEndpoint)
@@ -16,7 +26,7 @@ func InitGelfLog(logLevel slog.Level, gelfEndpoint string) {
 		slog.Error("Error creating GELF writer", "Error", err)
 	}
 	gelfWriter.CompressionType = gelf.CompressGzip
-	seqLogger := sloggraylog.Option{Level: logLevel, Writer: gelfWriter}.NewGraylogHandler()
+	seqLogger := sloggraylog.Option{Level: logLevel, Writer: gelfWriter, ReplaceAttr: replaceDuration}.NewGraylogHandler()
 	textLogger := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: logLevel})
 	logger := slog.New(slogmulti.Fanout(seqLogger, textLogger))
 	slog.SetDefault(logger.With("appname", "hubro"))
