97550cd6abb8799521780e48c0558bda32cd055b
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-01-21 20:05:24 +0100

    BasicAuth

--- modules/admin/admin.go
+++ modules/admin/admin.go
@@ -9,12 +9,24 @@ import (
 	"github.com/sokkalf/hubro/server"
 )
 
+func basicAuth(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if !ok || username != "admin" || password != "admin" {
+			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		h(w, r)
+	}
+}
+
 func Register(prefix string, h *server.Hubro, mux *http.ServeMux, options interface{}) {
 	slog.Info("Registering admin module")
-	mux.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/", basicAuth(func(w http.ResponseWriter, r *http.Request) {
 		h.RenderWithLayout(w, r, "admin/app", "admin/index", nil)
 	}))
-	mux.Handle("/ws", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/ws", basicAuth(func(w http.ResponseWriter, r *http.Request) {
 		conn, err := websocket.Accept(w, r, nil)
 		if err != nil {
 			slog.Error("Error accepting websocket connection", "error", err)
