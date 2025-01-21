48c94d95453e58db4ea5876871c4b91d5485ecc3
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-01-21 18:32:09 +0100

    WS send/receive test

--- modules/admin/admin.go
+++ modules/admin/admin.go
@@ -1,6 +1,7 @@
 package admin
 
 import (
+	"context"
 	"log/slog"
 	"net/http"
 
@@ -14,10 +15,24 @@ func Register(prefix string, h *server.Hubro, mux *http.ServeMux, options interf
 		w.Write([]byte("Hello, World!"))
 	}))
 	mux.Handle("/ws", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := websocket.Accept(w, r, nil)
+		conn, err := websocket.Accept(w, r, nil)
 		if err != nil {
 			slog.Error("Error accepting websocket connection", "error", err)
 			return
+		}
+		defer conn.CloseNow()
+		ctx := context.Background()
+
+		for {
+			t, b, err := conn.Read(ctx)
+			if err != nil {
+				// ...
+				slog.Error("Error reading message", "error", err)
+				return
+			}
+
+			slog.Debug("received message", "message", string(b), "type", t)
+			conn.Write(ctx, t, []byte("Hello, World!"))
 		}
 	}))
 }
