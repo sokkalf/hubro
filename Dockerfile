c99ef092ab03d7d8ed0dbb84e4328df559bd0bf7
Christian Lønaas <christian.lonaas@ugle-z.no>
2025-01-31 16:33:23 +0100

    Make downloading build tools in Dockerfile cacheable

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,15 @@
 FROM golang:1.23-alpine AS base
 RUN apk add gcompat build-base
+COPY scripts/setup_build.sh /scripts/setup_build.sh
+RUN /scripts/setup_build.sh
 WORKDIR /app
 COPY go.mod go.sum ./
 RUN go mod download && go mod verify
 COPY . /app
 ARG VERSION
 ENV REVISION=$VERSION
-RUN scripts/setup_build.sh
-RUN bin/tailwindcss-musl -i view/assets/css/app.css -m -o minified_app.css
-RUN bin/esbuild view/assets/js/app.js --minify --target=es2017 --bundle --outfile=minified_app.js
+RUN /buildtools/tailwindcss-musl -i view/assets/css/app.css -m -o minified_app.css
+RUN /buildtools/esbuild view/assets/js/app.js --minify --target=es2017 --bundle --outfile=minified_app.js
 RUN go build -ldflags="-s -w -X main.Version=$REVISION" -o /app/tmp/hubro
 
 FROM alpine:3.21 AS prod
--- scripts/setup_build.sh
+++ scripts/setup_build.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-if ! test -f bin/tailwindcss-musl; then
-	mkdir -p bin
+if ! test -f /buildtools/tailwindcss-musl; then
+	mkdir -p /buildtools
 	echo "Downloading Tailwind CLI (musl)"
-	wget https://github.com/tailwindlabs/tailwindcss/releases/download/v4.0.0/tailwindcss-linux-x64-musl -O bin/tailwindcss-musl
-	chmod +x bin/tailwindcss-musl
+	wget https://github.com/tailwindlabs/tailwindcss/releases/download/v4.0.0/tailwindcss-linux-x64-musl -O /buildtools/tailwindcss-musl
+	chmod +x /buildtools/tailwindcss-musl
 fi
 
-if ! test -f bin/esbuild ; then
-	mkdir -p bin
+if ! test -f /buildtools/esbuild ; then
+	mkdir -p /buildtools
 	echo "Downloading esbuild"
-	mkdir -p bin/tmp/
-	wget "https://registry.npmjs.org/@esbuild/linux-x64/-/linux-x64-0.24.1.tgz" -O bin/tmp/esbuild.tgz
-	tar -xzf bin/tmp/esbuild.tgz -C bin/
-	mv bin/package/bin/esbuild bin/esbuild
-	rm -rf bin/tmp
-	rm -rf bin/package
+	mkdir -p /tmp/bin/
+	wget "https://registry.npmjs.org/@esbuild/linux-x64/-/linux-x64-0.24.1.tgz" -O /tmp/esbuild.tgz
+	tar -xzf /tmp/esbuild.tgz -C /tmp/bin/
+	mv /tmp/bin/package/bin/esbuild /buildtools/esbuild
+	rm -rf /tmp/*
 fi
